449009a2adb839dbf77fdbcc417e948e7d5b1012
McClane Howland <mcclane.howland@gmail.com>
2020-04-04 13:31:20 -0700

    answer questions

---
+++ Syllabus.py
@@ -0,0 +1,14 @@
+def answer_question_1():
+    return 'Monday evening'
+
+def answer_question_2():
+    return 'Yes. You will struggle with some of the chapters without the textbook'
+
+def answer_question_3():
+    return 'Yes'
+
+def answer_question_4():
+    return 'Anaconda'
+
+def answer_question_5():
+    return "Yes. If you want to get anything higher than a C, you'll need to do more than the labs and assignments"
